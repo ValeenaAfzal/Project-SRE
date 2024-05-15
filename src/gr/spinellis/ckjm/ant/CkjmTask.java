44d63e786d6fe92258f67f845fb6b02fff1a7494
FatimaTahir12 <l201275@lhr.nu.edu.pk>
2024-05-15 17:07:36 +0500

    refactored code-fatima

--- src/gr/spinellis/ckjm/ant/CkjmTask.java
+++ src/gr/spinellis/ckjm/ant/CkjmTask.java
@@ -1,19 +1,3 @@
-/*
- * (C) Copyright 2005 Diomidis Spinellis, Julien Rentrop
- *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- */
-
 package gr.spinellis.ckjm.ant;
 
 import gr.spinellis.ckjm.MetricsFilter;
@@ -30,75 +14,40 @@ import org.apache.tools.ant.DirectoryScanner;
 import org.apache.tools.ant.taskdefs.MatchingTask;
 import org.apache.tools.ant.types.Path;
 
-/**
- * Ant task definition for the CKJM metrics tool.
- *
- * @version $Revision: 1.3 $
- * @author Julien Rentrop
- */
 public class CkjmTask extends MatchingTask {
-    private static final String FORMAT_PLAIN = "plain";//added
-    private static final String FORMAT_XML = "xml";//added
-    
     private File outputFile;
     private File classDir;
     private Path extdirs;
     private String format;
 
-     public CkjmTask() {
-        this.format = FORMAT_PLAIN; //added
+    public CkjmTask() {
+        this.format = "plain";
     }
 
-    /**
-     * Sets the format of the output file.
-     *
-     * @param format the format of the output file. Allowable values are 'plain' or 'xml'.
-     */
     public void setFormat(String format) {
         this.format = format;
     }
 
-    /**
-     * Sets the output file.
-     *
-     * @param outputFile Location of the output file
-     */
-    public void setOutputfile(File outputFile) {
-        this.outputFile = outputFile;
+    public void setOutputfile(File outputfile) {
+        this.outputFile = outputfile;
     }
 
-    /**
-     * Sets the directory which contains the class files that will be analyzed.
-     *
-     * @param classDir Location of class files
-     */
     public void setClassdir(File classDir) {
         this.classDir = classDir;
     }
 
-    /**
-     * Sets the extension directories that will be used by CKJM.
-     *
-     * @param extdirs a path containing .jar files
-     */
-    public void setExtdirs(Path extdirs) {
-        this.extdirs = extdirs;
+    public void setExtdirs(Path e) {
+        if (extdirs == null) {
+            extdirs = e;
+        } else {
+            extdirs.append(e);
+        }
     }
 
-    /**
-     * Gets the extension directories that will be used by CKJM.
-     *
-     * @return the extension directories as a path
-     */
     public Path getExtdirs() {
         return extdirs;
     }
 
-    /**
-     * Adds a path to extdirs.
-     *
-     * @return a path to be modified
-     */
     public Path createExtdirs() {
         if (extdirs == null) {
             extdirs = new Path(getProject());
@@ -106,54 +55,52 @@ public class CkjmTask extends MatchingTask {
         return extdirs.createPath();
     }
 
-    /**
-     * Executes the CKJM Ant Task.
-     *
-     * @throws BuildException if an error occurs.
-     */
-    public void execute() {
-        if (classDir == null) {
-            throw new BuildException("classdir attribute must be set!");
-        }
-        if (!classDir.exists()) {
-            throw new BuildException("classdir does not exist!");
-        }
-        if (!classDir.isDirectory()) {
-            throw new BuildException("classdir is not a directory!");
-        }
-
-        if (extdirs != null && extdirs.size() > 0) {
-            System.setProperty("java.ext.dirs", extdirs.toString());
-        }
+    public void execute() throws BuildException {
+        validateClassDir();
 
         DirectoryScanner ds = super.getDirectoryScanner(classDir);
-        String[] files = ds.getIncludedFiles();
+        String files[] = ds.getIncludedFiles();
+
         if (files.length == 0) {
             log("No class files in specified directory " + classDir);
         } else {
             for (int i = 0; i < files.length; i++) {
-                files[i] = new File(classDir, files[i]).getPath();
+                files[i] = classDir.getPath() + File.separatorChar + files[i];
             }
 
             try {
-                processMetrics(files, new FileOutputStream(outputFile));//added method call instead of sequential code
+                OutputStream outputStream = new FileOutputStream(outputFile);
+                PrintStream printStream = new PrintStream(outputStream);
+
+                if (format.equals("xml")) {
+                    printXmlMetrics(files, printStream);
+                } else {
+                    printPlainMetrics(files, printStream);
+                }
+
+                outputStream.close();
+
             } catch (IOException ioe) {
-                throw new BuildException("Error file handling: " + ioe.getMessage(), ioe);
+                throw new BuildException("Error file handling: " + ioe.getMessage());
             }
         }
     }
 
-    private void processMetrics(String[] files, OutputStream outputStream) throws IOException {//added this new method
-        try (OutputStream os = outputStream) {
-            if (FORMAT_XML.equals(format)) {
-                PrintXmlResults outputXml = new PrintXmlResults(new PrintStream(os));
-                outputXml.printHeader();
-                MetricsFilter.runMetrics(files, outputXml);
-                outputXml.printFooter();
-            } else {
-                PrintPlainResults outputPlain = new PrintPlainResults(new PrintStream(os));
-                MetricsFilter.runMetrics(files, outputPlain);
-            }
+    private void validateClassDir() {
+        if (classDir == null || !classDir.exists() || !classDir.isDirectory()) {
+            throw new BuildException("Invalid class directory: " + classDir);
         }
+    }
+
+    private void printXmlMetrics(String[] files, PrintStream printStream) {
+        PrintXmlResults outputXml = new PrintXmlResults(printStream);
+        outputXml.printHeader();
+        MetricsFilter.runMetrics(files, outputXml);
+        outputXml.printFooter();
+    }
+
+    private void printPlainMetrics(String[] files, PrintStream printStream) {
+        PrintPlainResults outputPlain = new PrintPlainResults(printStream);
+        MetricsFilter.runMetrics(files, outputPlain);
     }
 }
--- src/gr/spinellis/ckjm/ant/PrintXmlResults.java
+++ src/gr/spinellis/ckjm/ant/PrintXmlResults.java
@@ -1,18 +1,3 @@
-/*
- * (C) Copyright 2005 Diomidis Spinellis, Julien Rentrop
- *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- */
 package gr.spinellis.ckjm.ant;
 
 import gr.spinellis.ckjm.CkjmOutputHandler;
@@ -20,11 +5,6 @@ import gr.spinellis.ckjm.ClassMetrics;
 
 import java.io.PrintStream;
 
-/**
- * XML output formatter
- *
- * @author Julien Rentrop
- */
 public class PrintXmlResults implements CkjmOutputHandler {
     private final PrintStream printStream;
 
@@ -38,24 +18,24 @@ public class PrintXmlResults implements CkjmOutputHandler {
     }
 
     public void handleClass(String name, ClassMetrics classMetrics) {
-        printStream.printf("<class>\n" +
-                "<name>%s</name>\n" +
-                "<wmc>%d</wmc>\n" +
-                "<dit>%d</dit>\n" +
-                "<noc>%d</noc>\n" +
-                "<cbo>%d</cbo>\n" +
-                "<rfc>%d</rfc>\n" +
-                "<lcom>%d</lcom>\n" +
-                "<ca>%d</ca>\n" +
-                "<npm>%d</npm>\n" +
-                "</class>\n",
-                name, classMetrics.getWmc(), classMetrics.getDit(), classMetrics.getNoc(),
-                classMetrics.getCbo(), classMetrics.getRfc(), classMetrics.getLcom(),
-                classMetrics.getCa(), classMetrics.getNpm());
+        printStream.print(formatClassXml(name, classMetrics));
     }
 
-    public void printFooter() {
+    private String formatClassXml(String name, ClassMetrics classMetrics) {
+        return "<class>\n" +
+                "<name>" + name + "</name>\n" +
+                "<wmc>" + classMetrics.getWmc() + "</wmc>\n" +
+                "<dit>" + classMetrics.getDit() + "</dit>\n" +
+                "<noc>" + classMetrics.getNoc() + "</noc>\n" +
+                "<cbo>" + classMetrics.getCbo() + "</cbo>\n" +
+                "<rfc>" + classMetrics.getRfc() + "</rfc>\n" +
+                "<lcom>" + classMetrics.getLcom() + "</lcom>\n" +
+                "<ca>" + classMetrics.getCa() + "</ca>\n" +
+                "<npm>" + classMetrics.getNpm() + "</npm>\n" +
+                "</class>\n";
+    }
+
+    public void printFooter () {
         printStream.println("</ckjm>");
     }
 }
-
